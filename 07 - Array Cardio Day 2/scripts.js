84fcc8b5e01ba48ba44743ae6a5782ebb8a4aa43
Bss <mx.bss@proton.me>
2025-05-31 22:31:43 +0200

    done

--- 07 - Array Cardio Day 2/scripts.js
+++ 07 - Array Cardio Day 2/scripts.js
@@ -16,16 +16,28 @@ const comments = [
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
+console.table(people);
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const nineteen = people.some(p => 2025 - p.year >= 19);
+console.log(`Is anyone at least 19 years old ?\n${nineteen}`);
 // Array.prototype.every() // is everyone 19 or older?
+const allNineteens = people.every(p => 2025 - p.year >= 19);
+console.log(`Is everyone at least 19 years old ?\n${allNineteens}`)
 
+console.table(comments);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const id = comments.find(c => c.id == 823423);
+console.log(`ID 823423`)
+console.table(id);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
-
+const index = comments.findIndex(c => c.id === 823423);
+console.log(`index: ${index}`);
+comments.splice(index, 1);
+console.table(comments);
 
